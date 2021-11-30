9a6606d37ec172129bb06d13af8b3da8189767e8
sekikazuya <88357511+sekikazuya@users.noreply.github.com>
2021-11-30 10:08:48 +0900

    呼び出しシグネチャの使い方

--- src/function/basic.ts
+++ src/function/basic.ts
@@ -19,3 +19,17 @@ export const logMessage4 = (message: string): void =>   console.log('Function ba
 export const alwaysThrowError = (message: string): never => {
   throw new Error(message)
 }
+
+// 呼び出しシグネチャ（省略記法）
+type LogMessage = (message: string) => void
+export const logMessage6: LogMessage = (message) => {
+  console.log('Function basic sample 6:', message)
+}
+
+// 完全な呼び出しシグネチャ
+type FullLogMessage = {
+  (message: string): void
+}
+export const logMessage7: FullLogMessage = (message) => {
+  console.log('Function basic sample 7:', message)
+}
