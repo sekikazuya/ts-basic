5448cf19bbe96239b36b97a1e0a41af453e5450f
sekikazuya <88357511+sekikazuya@users.noreply.github.com>
2021-12-09 10:34:37 +0900

    インターフェースの継承と型エイリアスをインターフェースを使って拡張

--- src/interface/interface.ts
+++ src/interface/interface.ts
@@ -29,3 +29,35 @@ const maboDon: MaboDon = {
   spicyLevel: 10,
   gram: 350
 }
+
+// インターフェースの継承
+interface Book {
+  page: number
+  title: string
+}
+
+interface Magazine extends Book {
+  cycle: 'daily' | 'weekly' | 'monthly' | 'yearly'
+}
+
+const jump: Magazine = {
+  page: 300,
+  title: '週刊少年ジャンプ',
+  cycle: 'weekly'
+}
+
+// 型エイリアスをインターフェースを使って拡張
+type BookType = {
+  page: number
+  title: string
+}
+
+interface Handbook extends BookType {
+  theme: string
+}
+
+const cotrip: Handbook = {
+  page: 120,
+  title: 'ことりっぷ',
+  theme: '旅行'
+}
